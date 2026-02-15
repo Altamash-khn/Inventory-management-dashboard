764ba4d39e1b8f3e9e382c6c4a75cd9303da39ce
Abdul Rahman Khan <Abdulrkhan9899@gmail.com>
2026-02-15 21:24:16 +0530

    Updated signup functionality

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -13,10 +13,69 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function Login() {
-
+const navigate = useNavigate();
   const [show , setShow]= useState(true)
+  const [user , setUser] = useState({
+    email: "",
+    password: ""
+  })
+  
+ const handleLogin = async (e) => {
+
+  e.preventDefault();
+
+  if (!user.email.trim() || !user.password.trim()) {
+    alert("Username aur Password required hai");
+    return;
+  }
+
+  try {
+    const res = await fetch(
+      "https://inventory-management-backened.onrender.com/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: user.email,
+          password: user.password,
+        }),
+      }
+    );
+    const data = await res.json();
+    if (res.ok) {
+      localStorage.setItem("token",data.token)
+      navigate("/home");
+
+    } 
+    else if (res.status === 404) {
+      alert("User signup nahi hai, pehle signup karo");
+      navigate("/signup");
+    } 
+    else {
+      alert(data.message || "Invalid credentials");
+    }
+  } catch (error) {
+    console.log(error);
+    alert("Server error, baad me try karo");
+  }
+};
+    
+  
+  
+const handleChange = (e)=>{
+  setUser(prev => {
+    return{
+      ...prev,
+      [e.target.name]: e.target.value
+    }
+  } )
+  
+}
 
  const handleClick= ()=>{
   setShow(!show)
@@ -41,7 +100,10 @@ export function Login() {
               <Input
                 id="email"
                 type="email"
-                placeholder="Enter a email"
+                name='email'
+                placeholder="Enter your email"
+                value={user.email}
+                onChange={handleChange}
                 required
               />
             </div>
@@ -51,7 +113,7 @@ export function Login() {
                 
               </div>
               <div className="flex justify-center items-center  ">
-                <Input  id="password" type={show ? "password" : "text"} required placeholder="Enter a password"/>
+                <Input  id="password"   value={user.password} name="password" type={show ? "password" : "text"} required placeholder="Enter a password"  onChange={handleChange}/>
                 {show ?  <FaEyeSlash className="relative right-8" onClick={handleClick} /> :<FaEye onClick={handleClick} className="relative right-8"/>}
               
               </div>
@@ -60,7 +122,7 @@ export function Login() {
         </form>
       </CardContent>
       <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full">
+        <Button type="submit" className="w-full" onClick={handleLogin}>
           Login
         </Button>
         <p variant="outline" className="w-full text-gray-600  text-center">
