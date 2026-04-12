42dd9b9659564c6fe752afc76d0ceeede1c54632
kmskhn <khnkms@gmail.com>
2026-04-12 20:38:34 +0530

    refactored the complete project

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { Login } from "./components/login";
-import { Signup } from "./components/signupForm.jsx";
-import ProductForm from "./pages/Products.jsx";
-import HomePage from "./pages/Login.jsx";
+import Login from "./pages/Login.jsx";
+import SignUp from "./pages/SignUp.jsx";
 
 function App() {
   return (
     <Routes>
-      {/* Default Route */}
-      <Route path="/" element={<Navigate to="/HomePage" />} />x  x
-
-      <Route path="/HomePage" element={<HomePage />} />
-      <Route path="/products" element={<ProductForm/>} />
-
       <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 }
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { SignupComponent } from '../components/signupForm'
+
+const SignUp = () => {
+  return <SignupComponent />
+}
+
+export default SignUp
